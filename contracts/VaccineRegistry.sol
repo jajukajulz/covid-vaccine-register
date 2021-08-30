f7f851677f8d71b1cc00f92e2662fcc693eb3a90
Julian Kanjere <julian.kanjere@jskdezign.com>
2021-08-31 00:39:00 +0200

    Update after running into "CompilerError: Stack too deep, try removing local variables" when attempting to compile.

--- contracts/VaccineRegistry.sol
+++ contracts/VaccineRegistry.sol
@@ -5,8 +5,7 @@ contract VaccineRegistry {
   // define the struct VaccineRecord
   struct VaccineRecord {
     string identity_number;
-    string first_name;
-    string last_name;
+    string name_details; // first_name, last_name
     string vaccination_id;
     string vaccination_details; // vaccination_date, vaccine_name, vaccine_place
   }
@@ -21,13 +20,13 @@ contract VaccineRegistry {
   // define the array of vaccine records i.e. vaccinees
   VaccineRecord[] public vaccine_records;
 
-  function addVaccinationRecord(string calldata _identity_number, string calldata _first_name, string calldata _last_name,
+  function addVaccinationRecord(string calldata _identity_number, string calldata _name_details,
     string calldata _vaccination_id, string calldata _vaccination_details) external returns(uint){
 
     uint256 submissionBlockNumber = block.number;
 
     // get an instance of a VaccineRecord using the input variables and push into the array of vaccine_records, returns the id
-    uint id = vaccine_records.push(VaccineRecord(_identity_number, _first_name, _last_name, _vaccination_id, _vaccination_details)) - 1;
+    uint id = vaccine_records.push(VaccineRecord(_identity_number, _name_details, _vaccination_id, _vaccination_details)) - 1;
 
     // trigger event for VaccineRecord registration
     emit registeredVaccineRecordEvent(_identity_number, _vaccination_id, submissionBlockNumber);
